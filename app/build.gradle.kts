a33466f642ed8600edd421ca6966079eef0f4656
PocketSpace <xbu080675@gmail.com>
2026-09-17 22:48:38 +0800

    build: configure Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.xbu.videosourcedetection"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.xbu.videosourcedetection"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
